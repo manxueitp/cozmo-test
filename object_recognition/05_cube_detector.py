0449432a45440766921c0fad09e5f97cccf92d0d
manxueitp <mw3060@nyu.edu>
2017-05-03 17:27:08 -0700

    google object detect test

--- object_recognition/05_cube_detector.py
+++ object_recognition/05_cube_detector.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 import time, sys
 import dlib
 import numpy as np
--- object_recognition/google_vision_api.py
+++ object_recognition/google_vision_api.py
@@ -1,6 +1,30 @@
-import time, sys
-import dlib
+#import time, sys
+#import numpy as np
+#import cozmo
 
+import io
+import os
 
-import numpy as np
-import cozmo
+# Imports the Google Cloud client library
+from google.cloud import vision
+
+# Instantiates a client
+vision_client = vision.Client()
+
+# The name of the image file to annotate
+file_name = os.path.join(
+    os.path.dirname(__file__),
+    'img/1.jpg')
+
+# Loads the image into memory
+with io.open(file_name, 'rb') as image_file:
+    content = image_file.read()
+    image = vision_client.image(
+        content=content)
+
+# Performs label detection on the image file
+labels = image.detect_labels()
+
+print('Labels:')
+for label in labels:
+    print(label.description)
